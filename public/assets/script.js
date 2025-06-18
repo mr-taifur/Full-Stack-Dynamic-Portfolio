2636f7a61e5ae9d6730a7fb1831602ee3086b108
mr-taifur <taifurshishir123@gmail.com>
2025-06-18 12:02:10 +0600

    click+alert

---
+++ public/assets/script.js
@@ -0,0 +1,14 @@
+console.log(document.getElementById("test").innerText);
+
+tags = document.querySelector(".nav-links").children;
+for (let i = 0; i < tags.length; i++) {
+    tags[i].addEventListener('click', function (event) {
+        alert(event.target.innerText);
+    });
+}
+
+document.querySelectorAll(".title").forEach((abc) => {
+    abc.onclick = function () {
+        prompt(this.innerText);
+    };
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,51 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TAIFUR</title>
     <style>
-        body{
+        body {
             padding: 0;
             margin: 0;
+            background-image: url('assets/images/bg.jpg');
+            background-size: cover;
+            background-position: center center;
+            background-attachment: fixed;
         }
+
         .disp-flex {
             display: flex;
         }
+
         .flex-justify-space {
             justify-content: space-between;
+        }
 
+        .p-10 {
+            padding: 5px;
         }
-        .p-10{
-            padding: 10px;
+
+        nav {
+            background-color: aquamarine;
         }
-        nav{
-            background-color: aquamarine
-        }
-        .no-decor{
+
+        .no-decor {
             text-decoration: none;
         }
-        .no-decor:hover{
 
+        .no-decor:hover {}
+
+        img {
+            width: 50px;
+            height: auto;
+            margin-right: 10px;
         }
-       
+
+        h3 {
+            display: flex;
+            align-items: center;
+        }
+
+        section>.disp-flex.flex-justify-space>div {
+            color: white;
+        }
     </style>
 </head>
+
 <body>
     <nav>
         <div class="disp-flex flex-justify-space">
             <div>
                 <h3>
+
+                    <img src="{{ 'assets/images/taifur.png' }}" alt="">
                     Taifur
                 </h3>
             </div>
-            <div>
+            <div class="nav-links">
                 <a class="p-10 no-decor" href="#">About</a>
                 <a class="p-10 no-decor" href="#">Research Projects</a>
                 <a class="p-10 no-decor" href="#">Blog</a>
                 <a class="p-10 no-decor" href="#">Contact</a>
-                <a class="p-10 no-decor" href="#">Linked IN</a>
+                <a class="p-10 no-decor" href="#">Linkedin</a>
                 <a class="p-10 no-decor" href="#">Git</a>
                 <a class="p-10 no-decor" href="#">Resume</a>
             </div>
@@ -54,9 +79,6 @@
     <section>
         <div class="disp-flex flex-justify-space">
             <div>
-                <img src="{{('assets/images/taifur.png')}}" alt="">
-            </div>
-            <div class="">
                 About
             </div>
         </div>
@@ -65,9 +87,13 @@
                 More About
             </div>
             <div>
-                My Projects
+                <h1 class="title">My Projects</h1>
+                <p id="test">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, velit.</p>
             </div>
         </div>
     </section>
+    <script src="{{ asset('js/script.js') }}"></script>
+
 </body>
+
 </html>
