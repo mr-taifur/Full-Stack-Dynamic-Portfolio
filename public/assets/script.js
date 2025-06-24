6ba1a7b211ffa7daaa9b2375d719197e62b3a3a6
mr-taifur <taifurshishir123@gmail.com>
2025-06-24 22:14:56 +0600

    updated

--- public/assets/script.js
+++ public/assets/script.js
@@ -9,17 +9,6 @@ for (let i = 0; i < tags.length; i++) {
 
 document.querySelectorAll(".title").forEach((abc) => {
     abc.onclick = function () {
-        this.innerHTML = `
-        <h3>
-            <a href="#"> This is by js</a>
-            <p> this is assisgned dynamically</p>
-        <\h3>
-        `;
-        this.classList.add("card");
-
-        avatar = document.getElementById("avatar");
-        avatar.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thebullvine.com%2Fmanagement%2Fai-changes-from-artificial-insemination-to-artificial-intelligence-on-the-dairy-farm%2F&psig=AOvVaw3WIuT5dzTv3Pq3d_bf2mRx&ust=1750314416015000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMi1hLqr-o0DFQAAAAAdAAAAABAE"
-        avatar.classList.add("card");
+        prompt(this.innerText);
     };
 });
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TAIFUR</title>
+    <title>taifur's Portfolio</title>
     <style>
-        body {
+         body {
             padding: 0;
             margin: 0;
-            background-image: url('{{ asset('assets/images/bg.jpg') }}');
             background-size: cover;
             background-position: center center;
             background-attachment: fixed;
@@ -24,64 +23,59 @@
             justify-content: space-between;
         }
 
-        .p-10 {
-            padding: 5px;
+        .p {
+            padding: 10px;
         }
 
-        nav {
-            background-color: aquamarine;
-        }
+       nav {
+    background-color: aquamarine;
+    padding: 5px 15px;
+}
 
-        .no-decor {
-            text-decoration: none;
-        }
+h3 {
+    display: flex;
+    align-items: center; 
+    margin: 0;
+}
 
-        img {
-            width: 50px;
-            height: auto;
-            margin-right: 10px;
-        }
+img {
+    width: 50px;
+    height: auto;
+    margin-right: 10px;
+    vertical-align: middle; 
+}
 
-        h3 {
-            display: flex;
-            align-items: center;
-        }
-
-        section>.disp-flex.flex-justify-space>div {
-            color: white;
-        }
     </style>
 </head>
 
 <body>
-    <nav>
-        <div class="disp-flex flex-justify-space">
-            <div>
-                <h3>
-                    <img id = "avatar" src="{{ asset('assets/images/taifur.png') }}" alt="logo">
-                    Taifur
-                </h3>
-            </div>
-            <div class="nav-links">
-                <a class="p-10 no-decor" href="#">About</a>
-                <a class="p-10 no-decor" href="#">Research Projects</a>
-                <a class="p-10 no-decor" href="#">Blog</a>
-                <a class="p-10 no-decor" href="#">Contact</a>
-                <a class="p-10 no-decor" href="#">Linkedin</a>
-                <a class="p-10 no-decor" href="#">Git</a>
-                <a class="p-10 no-decor" href="#">Resume</a>
-            </div>
+     <nav>
+    <div class="disp-flex flex-justify-space">
+        <div>
+            <h3>
+                <img id="avatar" src="{{ asset('assets/images/taifur.png') }}" alt="logo">
+                Taifur
+            </h3>
         </div>
+        <div class="nav-links">
+            <a class="p-10 no-decor" href="#">About</a>
+            <a class="p-10 no-decor" href="#">Research Projects</a>
+            <a class="p-10 no-decor" href="#">Blog</a>
+            <a class="p-10 no-decor" href="#">Contact</a>
+            <a class="p-10 no-decor" href="#">Linkedin</a>
+            <a class="p-10 no-decor" href="#">Git</a>
+            <a class="p-10 no-decor" href="#">Resume</a>
+        </div>
+    </div>
+</nav>
     </nav>
     <section>
-        <div class="disp-flex flex-justify-space">
-            <div>
-                <h1 class="title">My Projects</h1>
-                <p id="test">hello im taifur</p>
-            </div>
+            <h1 class="title">Hello I am Taifur</h1>
+            <p id="test">Welcome to My portfolio</p>
         </div>
     </section>
-    <script src="{{ asset('js/script.js') }}"></script>
+
+    <script src="{{ asset('assets/script.js') }}"></script>
 </body>
 
 </html>
